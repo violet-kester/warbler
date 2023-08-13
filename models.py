0d591a55c39c96fa2aa8dde502c7a5d13c0235c9
Violet Kester <kestervj89@gmail.com>
2023-08-13 18:30:16 -0400

    change single quotes, whitespace

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-"""SQLAlchemy models for Warbler."""
+'''SQLAlchemy models for Warbler.'''
 
 from datetime import datetime
 
@@ -8,30 +8,31 @@ from flask_sqlalchemy import SQLAlchemy
 bcrypt = Bcrypt()
 db = SQLAlchemy()
 
-DEFAULT_IMAGE_URL = "/static/images/default-pic.png"
-DEFAULT_HEADER_IMAGE_URL = "/static/images/warbler-hero.jpg"
+DEFAULT_IMAGE_URL = '/static/images/default-pic.png'
+DEFAULT_HEADER_IMAGE_URL = '/static/images/warbler-hero.jpg'
 
 
 class Follows(db.Model):
-    """Connection of a follower <-> followed_user."""
+    '''Connection of a follower <-> followed_user.'''
 
     __tablename__ = 'follows'
 
     user_being_followed_id = db.Column(
         db.Integer,
-        db.ForeignKey('users.id', ondelete="cascade"),
+        db.ForeignKey('users.id', ondelete='cascade'),
         primary_key=True,
     )
 
     user_following_id = db.Column(
         db.Integer,
-        db.ForeignKey('users.id', ondelete="cascade"),
+        db.ForeignKey('users.id', ondelete='cascade'),
         primary_key=True,
     )
 
+
 # TODO: length constraints?
 class User(db.Model):
-    """User in the system."""
+    '''User in the system.'''
 
     __tablename__ = 'users'
 
@@ -75,30 +76,30 @@ class User(db.Model):
         nullable=False,
     )
 
-    messages = db.relationship('Message', backref="user")
+    messages = db.relationship('Message', backref='user')
 
     followers = db.relationship(
-        "User",
-        secondary="follows",
+        'User',
+        secondary='follows',
         primaryjoin=(Follows.user_being_followed_id == id),
         secondaryjoin=(Follows.user_following_id == id),
-        backref="following",
+        backref='following',
     )
 
     liked_messages = db.relationship(
-        "Message",
-        secondary = "likes"
+        'Message',
+        secondary = 'likes'
     )
 
     def __repr__(self):
-        return f"<User #{self.id}: {self.username}, {self.email}>"
+        return f'<User #{self.id}: {self.username}, {self.email}>'
 
     @classmethod
     def signup(cls, username, email, password, image_url=DEFAULT_IMAGE_URL):
-        """Sign up user.
+        '''Sign up user.
 
         Hashes password and adds user to system.
-        """
+        '''
 
         hashed_pwd = bcrypt.generate_password_hash(password).decode('UTF-8')
 
@@ -114,7 +115,7 @@ class User(db.Model):
 
     @classmethod
     def authenticate(cls, username, password):
-        """Find user with `username` and `password`.
+        '''Find user with `username` and `password`.
 
         This is a class method (call it on the class, not an individual user.)
         It searches for a user whose password hash matches this password
@@ -122,33 +123,35 @@ class User(db.Model):
 
         If this can't find matching user (or if password is wrong), returns
         False.
-        """
+        '''
 
         user = cls.query.filter_by(username=username).first()
 
         if user:
             is_auth = bcrypt.check_password_hash(user.password, password)
             if is_auth:
+                print('user login', user)
                 return user
 
         return False
 
     def is_followed_by(self, other_user):
-        """Is this user followed by `other_user`?"""
+        '''Is user followed by other_user?'''
 
         found_user_list = [
             user for user in self.followers if user == other_user]
         return len(found_user_list) == 1
 
     def is_following(self, other_user):
-        """Is this user following `other_user`?"""
+        '''Is user following other_user?'''
 
         found_user_list = [
             user for user in self.following if user == other_user]
         return len(found_user_list) == 1
 
+
 class Message(db.Model):
-    """An individual message ("warble")."""
+    '''An individual message ('warble').'''
 
     __tablename__ = 'messages'
 
@@ -174,28 +177,30 @@ class Message(db.Model):
         nullable=False,
     )
 
+
 class Like(db.Model):
-    """Connection of a user <-> liked message."""
+    '''Connection of a user <-> liked message.'''
 
     __tablename__ = 'likes'
 
     user_id= db.Column(
         db.Integer,
-        db.ForeignKey('users.id', ondelete="cascade"),
+        db.ForeignKey('users.id', ondelete='cascade'),
         primary_key=True,
     )
 
     message_id = db.Column(
         db.Integer,
-        db.ForeignKey('messages.id', ondelete="cascade"),
+        db.ForeignKey('messages.id', ondelete='cascade'),
         primary_key=True,
     )
 
+
 def connect_db(app):
-    """Connect this database to provided Flask app.
+    '''Connect this database to provided Flask app.
 
     You should call this in your Flask app.
-    """
+    '''
 
     app.app_context().push()
     db.app = app
